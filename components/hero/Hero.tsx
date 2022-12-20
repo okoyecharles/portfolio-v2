b1557441f094f2b644b936728767e7626bfd79b7
Charles <okoyecharles509@gmail.com>
2022-12-20 16:40:46 +0100

    Animate hero page

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,27 +1,48 @@
-import React, { useRef } from 'react'
-import Section from '../main/Section'
+import React, { useEffect } from "react";
+import Section from "../main/Section";
+import { useTrail, a, config } from "@react-spring/web";
 
 interface heroProps {
-  heroRef: React.MutableRefObject<null>
+  heroRef: React.MutableRefObject<null>;
 }
 
-useRef
 const Hero: React.FC<heroProps> = ({ heroRef }) => {
+  const [springs, api] = useTrail(4, () => ({
+    from: {
+      y: 50,
+      opacity: 0,
+    },
+    config: {
+      tension: 235,
+      friction: 28,
+    },
+  }));
+
+  // Start animation after a second
+  useEffect(() => {
+    setTimeout(() => {
+      api.start({
+        y: 0,
+        opacity: 1,
+      });
+    }, 1000);
+  }, []);
+
   return (
     <Section label="hero" id="content" sectionRef={heroRef}>
-          <h1>Hi, my name is</h1>
-          <h2>Okoye Charles.</h2>
-          <h3>I embrace the digital world.</h3>
-          <p>
-            I can help you build a product, feature, or website Look through
-            some of my work and experience! If you like what you see and need
-            help on a project, I am available for hire.
-          </p>
-          <a href="#featured">
-            <button tabIndex={-1}>Check out my work!</button>
-          </a>
-        </Section>
-  )
-}
+      <a.h1 style={springs[0]}>Hi, my name is</a.h1>
+      <a.h2 style={springs[0]}>Okoye Charles.</a.h2>
+      <a.h3 style={springs[1]}>I embrace the digital world.</a.h3>
+      <a.p style={springs[2]}>
+        I can help you build a product, feature, or website Look through some of
+        my work and experience! If you like what you see and need help on a
+        project, I am available for hire.
+      </a.p>
+      <a.a href="#featured" style={springs[3]}>
+        <button tabIndex={-1}>Check out my work!</button>
+      </a.a>
+    </Section>
+  );
+};
 
-export default Hero
+export default Hero;
