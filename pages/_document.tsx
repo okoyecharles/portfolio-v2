1906f641430a1a83740d1a605f61d56932365597
Charles <okoyecharles509@gmail.com>
2022-12-22 08:24:02 +0100

    Add google verification script (Analytics)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T9973SH7JN"></script>
+        <script dangerouslySetInnerHTML={{__html: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-T9973SH7JN');"}} />
         <script dangerouslySetInnerHTML={{ __html: "const webConfig=JSON.parse(localStorage.getItem('okoye-charles-web-config')||'{}'),root=document.querySelector(':root');webConfig.darkMode&&root?.classList.add('dark');" }}/>
       </Head>
       <body>
