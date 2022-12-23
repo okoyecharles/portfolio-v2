a1a811a730d205b4592cc78d16c3a036bd18afd2
Charles <okoyecharles509@gmail.com>
2022-12-23 21:29:09 +0100

    Update script tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-T9973SH7JN"></script>
-        <script dangerouslySetInnerHTML={{__html: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-T9973SH7JN');"}} />
-        <script dangerouslySetInnerHTML={{ __html: "const webConfig=JSON.parse(localStorage.getItem('okoye-charles-web-config')||'{}'),root=document.querySelector(':root');webConfig.darkMode&&root?.classList.add('dark');" }}/>
+        <Script strategy='beforeInteractive'>
+          {`
+            const webConfig = JSON.parse(localStorage.getItem('okoye-charles-web-config') || '{}'),root=document.querySelector(':root');
+            webConfig.darkMode && root?.classList.add('dark');
+          `}
+        </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T9973SH7JN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            
+            gtag('config', 'G-T9973SH7JN');
+          `}
+        </Script>
       </Head>
       <body>
         <Main />
