0e6f500995a2b8dc361ca3a1040735a1b339d251
Charles <okoyecharles509@gmail.com>
2022-12-31 09:28:32 +0100

    Test contact form

---
+++ components/contact/ContactForm.cy.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ContactForm from './ContactForm'
+
+describe('<ContactForm />', () => {
+  it('renders', () => {
+    cy.mount(<ContactForm />)
+  });
+  
+  it('contains right inputs', () => {
+    cy.mount(<ContactForm />)
+
+    // Type in content to inputs
+    cy.get('#contactForm__name').type('Okoye Charles');
+    cy.get('#contactForm__email').type('okoyecharles509@gmail.com');
+
+    // Intentionally type too short text in textarea
+    cy.get('#contactForm__message').type('Hey c');
+
+    // Submit form
+    cy.get('.contactForm > button[type="submit"]').click();
+    
+    // Check for error
+    cy.get('#contactForm__message').should('have.class', 'invalid');
+  })
+})
