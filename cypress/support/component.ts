c51dd84e5277f2fe4f7054b47af8a1508149ceda
Charles <okoyecharles509@gmail.com>
2022-12-31 10:05:17 +0100

    Resolve build errors

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -25,13 +25,13 @@ import { mount } from 'cypress/react18'
 // your custom command.
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount
-    }
-  }
-}
+// declare global {
+//   namespace Cypress {
+//     interface Chainable {
+//       mount: typeof mount
+//     }
+//   }
+// }
 
 Cypress.Commands.add('mount', mount)
 
