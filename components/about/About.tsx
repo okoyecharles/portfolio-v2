c0ed2c086884114493b964ae87e05f8b5d7e0553
Charles <okoyecharles509@gmail.com>
2023-09-11 15:41:10 +0100

    Fix grammatical issues in About section

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -23,9 +23,9 @@ const About: React.FC<AboutProps> = ({ aboutRef }) => {
   useIntersectionObserver(aboutRef, () => {
     api.start({
       y: 0,
-      opacity: 1
-    })
-  })
+      opacity: 1,
+    });
+  });
 
   return (
     <Section label="about" sectionRef={aboutRef}>
@@ -35,45 +35,37 @@ const About: React.FC<AboutProps> = ({ aboutRef }) => {
       <a.div className="about__container" style={springs[1]}>
         <article className="about__content">
           <p>
-            Hello! My name is <b>Okoye Charles Kaosisochukwu</b> and I&apos;m a
-            creative developer who loves to work on appealing websites with a
-            passion for User Interfaces and Experience. I love spending my time
-            coding, creating things for the web and learning new things.
+            I am Okoye Charles, a frontend/backend developer based in Nigeria. I
+            spend most of my time designing graphics, coding up things for the
+            web, and learning algorithms.
           </p>
 
           <p>
-            I&apos;m relatively new to the software development world and since May
-            2022 I&apos;ve been building my technical, leadership, and collaborative
-            skills at{" "}
-            <a href="https://www.microverse.org/?grsf=wtedvt" target="_blank">
-              Microverse
-            </a>{" "}
-            through collaborating and pair programming with other students from
-            all over the world.
+            My goal is to deliver—through code—unique, and innovative solutions
+            to complex problems.
           </p>
 
           <p>
-            My main aim is to create and deliver unique, high-end and innovative
-            products for a variety of clients. If you are in need of an
-            experienced Software Engineer, I am{" "}
-            <a href="mailto:okoyecharles509@gmail.com?subject=Hire%20Okoye%20Charles!">
-              available for hire.
-            </a>
+            If my portfolio interests you, or you need an enthusiastic developer
+            on your team, I am{" "}
+            <a href="mailto:okoyecharles509@gmail.com">available for hire.</a>
           </p>
 
-          <p>Here are a few technologies I have been working with recently:</p>
+          <p>
+            Here is an up-to-date list of my most recently used technologies:
+          </p>
 
           <ul className="about__techList">
-            <li>Javascript (ES6+)</li>
-            <li>Next.js 13</li>
+            <li>Javascript (ES6+)/TypeScript</li>
             <li>React</li>
-            <li>TypeScript</li>
-            <li>Ruby</li>
+            <li>Next.js 13</li>
+            <li>Express.js (MongoDB)</li>
+            <li>Firebase</li>
             <li>Ruby on Rails</li>
           </ul>
 
           <span className="extra-info">
-            HAVE A COOL WEBSITE OR APPLICATION IN MIND? I&apos;D LOVE TO{" "}
+            DO YOU HAVE A COOL WEBSITE OR APPLICATION IN MIND? <br />I WOULD LOVE TO{" "}
             <a href="#contact">HEAR ABOUT IT</a>
           </span>
         </article>
